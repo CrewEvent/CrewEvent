05f2082aa88e14f08e65910f601f6df006ee1f85
gloatingorc <gloatingorc@gmail.com>
2022-11-04 11:27:02 +0100

    ajout authentification et page d'acceuil

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,7 +8,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
+#[ORM\Table(name: '`users`')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
