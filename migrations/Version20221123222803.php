22be3904b6bc35bfd1991ab6b24fbe4e558a5b2d
delmo128 <e2palmes@gmail.com>
2022-11-23 23:51:24 +0100

    added the remove contact functionnality

--- migrations/Version20221123222803.php
+++ migrations/Version20221123222803.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221122232601 extends AbstractMigration
+final class Version20221123222803 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,11 +22,15 @@ final class Version20221122232601 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE contacts (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, username VARCHAR(255) NOT NULL, contact_username VARCHAR(255) NOT NULL, INDEX IDX_33401573A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE contacts ADD CONSTRAINT FK_33401573A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE contact DROP FOREIGN KEY FK_4C62E638A76ED395');
+        $this->addSql('DROP TABLE contact');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, INDEX IDX_4C62E638A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE contact ADD CONSTRAINT FK_4C62E638A76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->addSql('ALTER TABLE contacts DROP FOREIGN KEY FK_33401573A76ED395');
         $this->addSql('DROP TABLE contacts');
     }
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -14,7 +14,7 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact/{username}', name: 'add_contact')]
+    #[Route('/contact/add/{username}', name: 'add_contact')]
     public function add_contact(User $user, EntityManagerInterface $em)
     {
 
@@ -33,6 +33,19 @@ class ContactController extends AbstractController
 
         //On met dans la base de donnée
         $em->persist($contact);
+        $em->flush($contact);
+
+        //puis on redirigige dans la page d'acceuil ?? à revoir
+        return $this->redirectToRoute('app_index');
+    }
+
+    #[Route('/contact/remove/{username}', name: 'remove_contact')]
+    public function remove_contact(User $user, ContactRepository $contactRepo, EntityManagerInterface $em)
+    {
+        $contact = $contactRepo->findOneBy(['user' => $user->getId(), 'username' => $this->getUser()->getUserIdentifier()]);
+
+        //On retire le contact de la base de donnée
+        $em->remove($contact);
         $em->flush($contact);
 
         //puis on redirigige dans la page d'acceuil ?? à revoir
