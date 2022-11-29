2f83de6623bd08e74aecfec9334c1babc484c1d9
gloatingorc <gloatingorc@gmail.com>
2022-11-29 17:10:43 +0100

    recherche par nom événements ou par username

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Repository\EventRepository;
 use App\Repository\UserRepository;
-use PhpParser\Builder\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\EventRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,18 +14,21 @@ class RechercheController extends AbstractController
 {
     //Controller ppour gérer la partie recherche
 
-    #[Route('/recherche', name: 'app_recherche', methods: ['post', 'get'])]
-    public function recherche(Request $request): Response
+    #[Route('/recherche', name: 'app_recherche', methods: ['get', 'post'])]
+    public function recherche(Request $request, EventRepository $eventRepo, UserRepository $userRepo): Response
     {
 
         //Je prend les données du formulaire de recherche de la navbar
-        $data = $request->request->all()['recherche'];
+        $data = $request->query->get('recherche');
 
 
-        // $events = $eventRepo->findBy(['name' => $data]);
+        $events = $eventRepo->findBy(['name' => $data]);
 
-        // $users = $userRepo->findBy(['username' => $data]);
+        $users = $userRepo->findBy(['username' => $data]);
 
-        return $this->render('recherche/recherche.html.twig');
+        return $this->render('recherche/recherche.html.twig', [
+            'events' => $events,
+            'users' => $users
+        ]);
     }
 }
