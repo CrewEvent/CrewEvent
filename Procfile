2fa1a72bbf584c23bcb42175882a5971959f5fce
delmo128 <e2palmes@gmail.com>
2022-12-03 15:28:04 +0100

    added search contacts functionnality

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-release: php bin/console cache:clear && php bin/console cache:warmup && php bin/console doctrine:migrations:migrate --no-interaction
+release: php bin/console cache:clear && php bin/console cache:warmup && php bin/console doctrine:migrations:migrate --no-interaction && $env:MERCURE_PUBLISHER_JWT_KEY='!ChangeMe!'; $env:MERCURE_SUBSCRIBER_JWT_KEY='!ChangeMe!';$env:SERVER_NAME='localhost:3014'; .\mercure\mercure.exe run -config .\mercure\Caddyfile.dev
 web: heroku-php-apache2 public/
+worker: * * * * * bin/console messenger:consume async --memory-limit=128M
--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -4,28 +4,44 @@ namespace App\Controller;
 
 use App\Repository\EventRepository;
 use App\Repository\UserRepository;
-use PhpParser\Builder\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\EventRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 class RechercheController extends AbstractController
 {
     //Controller ppour gérer la partie recherche
 
-    #[Route('/recherche', name: 'app_recherche', methods: ['post', 'get'])]
-    public function recherche(Request $request): Response
+    #[Route('/recherche', name: 'app_recherche', methods: ['get', 'post'])]
+    public function recherche(Request $request, EventRepository $eventRepo, UserRepository $userRepo, PaginatorInterface $paginator): Response
     {
 
         //Je prend les données du formulaire de recherche de la navbar
-        $data = $request->request->all()['recherche'];
+        $data = $request->query->get('recherche');
+
+        //On recherche dans colonne nom et puis dans la colonne tag
+        $allevents = $eventRepo->findBySearch($data);
+
+        //On recherche dans la colonne identifiant, prenom et nom
+        $users = $userRepo->findBySearch($data);
+
+        // Paginate the results of the query
+        $events = $paginator->paginate(
+            // Doctrine Query, not results
+            $allevents,
+            // Define the page parameter
+            $request->query->getInt('page', 1),
+            // Items per page
+            10
+        );
 
 
-        // $events = $eventRepo->findBy(['name' => $data]);
-
-        // $users = $userRepo->findBy(['username' => $data]);
-
-        return $this->render('recherche/recherche.html.twig');
+        return $this->render('recherche/recherche.html.twig', [
+            'events' => $events,
+            'users' => $users
+        ]);
     }
 }
