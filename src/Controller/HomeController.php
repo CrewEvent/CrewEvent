d50be108ebd9810740689fe65e22eccd7fcd8dd7
gloatingorc <gloatingorc@gmail.com>
2022-12-21 20:25:24 +0100

    correction big suggestions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,9 +15,9 @@ class HomeController extends PublicationController
 {
     #[Route('/home', name: 'app_home')]
     public function home(PublicationRepository $postRepo,
-                         EventRepository $eventRepo,
+                         EventRepository       $eventRepo,
                          ParticipantRepository $participantRepo
-                                                                ): Response
+    ): Response
     {
         $participants = $participantRepo->findBy(['participantUsername' => $this->getUser()->getUserIdentifier()]);
 
@@ -28,7 +28,7 @@ class HomeController extends PublicationController
         //Je fais ici la suggestion d'événements
         //Je récupére la liste des tags des événements participés par l'utilisateurs dans une liste
         $tags = [];
-        foreach ($participants as $participant){
+        foreach ($participants as $participant) {
             array_push($tags, $participant->getEvent()->getTag());
         }
 
@@ -39,45 +39,50 @@ class HomeController extends PublicationController
         arsort($tags);
 
         //Je prend les 2 premiers tags dans une list des tags
-        $fav = array_keys(array_slice($tags, 0,2));
+        $fav = array_keys(array_slice($tags, 0, 2));
+
 
         //On récupére les événements qui ont pour tag ces tags favories
         $favs = $eventRepo->findfavEvent($fav);
 
+
         $events = [];
         //On filtre les événements qui on t plus de participants
-        foreach ($favs as $event){
+        foreach ($favs as $event) {
 
             array_push($events, [$event->getName(), $event->getParticipants()->count()]);
         }
+
 
         //On trie la liste des événements du plus petit nbre de participants au plus grand
         $events = $this->selection_sort($events);
 
 
         //On prend les 3 derniers
-        $suggestions = array_slice($events, -3,3);
+        $suggestions = array_slice($events, -3, 3);
 
         //S'il a déja participé on ne va pas lui suggérer encore
-        foreach ($participants as $participant){
-            $i=0;
-            foreach ($suggestions as $suggestion){
 
-                if($suggestion[0] == $participant->getEventName()){
-                    unset($suggestions[$i]);
+
+        foreach ($participants as $participant) {
+            foreach ($suggestions as $key => $suggestion){
+             if ($suggestion[0] == $participant->getEventName()) {
+                    unset($suggestions[$key]);
+                    //On réindex les valeurs
+                    $suggestions = array_values($suggestions);
                 }
-                $i++;
             }
         }
 
         return $this->render('home/home.html.twig', [
             'post' => $post,
-            'participants'=>$participants,
+            'participants' => $participants,
             'suggestions' => $suggestions
         ]);
     }
 
-    function swap_positions($data1, $left, $right) {
+    function swap_positions($data1, $left, $right)
+    {
         $backup_old_data_right_value = $data1[$right];
         $data1[$right] = $data1[$left];
         $data1[$left] = $backup_old_data_right_value;
@@ -86,11 +91,11 @@ class HomeController extends PublicationController
 
     function selection_sort($data)
     {
-        for($i=0; $i<count($data)-1; $i++) {
+        for ($i = 0; $i < count($data) - 1; $i++) {
             $min = $i;
-            for($j=$i+1; $j<count($data); $j++) {
+            for ($j = $i + 1; $j < count($data); $j++) {
 
-                if ($data[$j][1]<$data[$min][1]) {
+                if ($data[$j][1] < $data[$min][1]) {
                     $min = $j;
                 }
             }
