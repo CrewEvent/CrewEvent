01883cc1dca471f6d8092ec1558c73ecee29839e
Meutevive <christianmbappe033@gmail.com>
2022-12-22 17:26:21 +0100

    ✨ rien à commit

--- migrations/Version20221222151404.php
+++ migrations/Version20221222151404.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221221190152 extends AbstractMigration
+final class Version20221222151404 extends AbstractMigration
 {
     public function getDescription(): string
     {
